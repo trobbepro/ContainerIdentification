1f3b0897f322caf466a3b56e39ae8e75b57afb73
Thomas Robbe <thomas.robbe@innoloop.eu>
2023-09-08 11:32:26 +0200

    improve tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.razor", "./**/*.cshtml", "./**/*.css"],
+    content: ["index.html"],
     theme: {
         screens: {
             sm: "768px",
@@ -24,6 +24,10 @@ module.exports = {
                 xl: '1128px',
                 '2xl': '1312px',
             },
+        },
+        fontFamily: {
+            'lexend': ['Lexend Deca', 'sans-serif'],
+            'michroma': ['Michroma', 'sans-serif'],
         },
         colors: {
             primary: {
@@ -67,24 +71,34 @@ module.exports = {
             },
         },
         spacing: {
-            0.25: "0.125rem", // 2px en rem
-            0.5: "0.25rem", // 4px en rem
-            0.75: "0.375rem", // 6px en rem
-            1: "0.5rem", // 8px en rem
-            1.5: "0.75rem", // 12px en rem
-            2: "1rem", // 16px en rem
-            2.5: "1.25rem", // 20px en rem
-            3: "1.5rem", // 24px en rem
-            4: "2rem", // 32px en rem
-            5: "2.5rem", // 40px en rem
-            6: "3rem", // 48px en rem
-            7: "3.5rem", // 56px en rem
-            8: "4rem", // 64px en rem
-            9: "4.5rem", // 72px en rem
-            10: "5rem", // 80px en rem
-            11: "5.5rem", // 88px en rem
-            12: "6rem", // 96px en rem
-            13: "6.5rem", //104px en rem
+            "0.25": "0.125rem", // 2px en rem
+            "0.5": "0.25rem", // 4px en rem
+            "0.75": "0.375rem", // 6px en rem
+            "1": "0.5rem", // 8px en rem
+            "1.5": "0.75rem", // 12px en rem
+            "2": "1rem", // 16px en rem
+            "2.5": "1.25rem", // 20px en rem
+            "3": "1.5rem", // 24px en rem
+            "4": "2rem", // 32px en rem
+            "5": "2.5rem", // 40px en rem
+            "6": "3rem", // 48px en rem
+            "7": "3.5rem", // 56px en rem
+            "8": "4rem", // 64px en rem
+            "9": "4.5rem", // 72px en rem
+            "10": "5rem", // 80px en rem
+            "11": "5.5rem", // 88px en rem
+            "12": "6rem", // 96px en rem
+            "13": "6.5rem", //104px en rem
+            "none": "0",
+            "3xs": "0.125rem",
+            "2xs": "0.25rem",
+            "xs": "0.5rem",
+            "s": "1rem",
+            "m": "1.5rem",
+            "l": "2rem",
+            "xl": "2.5rem",
+            "2xl": "3.5rem",
+            "3xl": "5rem"
         },
         borderRadius: {
             xs: "0.25rem",
@@ -92,17 +106,30 @@ module.exports = {
             m: "1rem",
             l: "1.5rem",
             xl: "8rem",
+            "modal": "0.25rem",
+            "button": "0.25rem",
+            "selection-card": "0.25rem",
+            "button-card": "0.25rem",
+            "input": "0.25rem",
+            "card": "1.5rem"
         },
         fontSize: {
-            sm: '0.8rem',
+            xs: '0.75rem',
+            sm: '0.875rem',
             base: '1rem',
-            xl: '1.25rem',
-            '2xl': '1.563rem',
-            '3xl': '1.953rem',
-            '4xl': '2.441rem',
-            '5xl': '3.052rem',
+            l: '1.25rem',
+            xl: '1.5rem',
+            '2xl': '2.25rem',
+            '3xl': '2.5rem',
         },
-        extend: {},
+        extend: {
+            textColor: {
+                'primary': "#191B1F",
+                'placeholder': "#4E5974",
+                'disabled-button': "#8892A9",
+                'disabled': "#BBC1D1"
+            }
+        },
     },
     plugins: [
         require('@tailwindcss/forms'),
